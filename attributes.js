e00b5679a0bf11176bb046ddd8b0712ade4dea03
thenellb <thenellb@oregonstate.edu>
2018-06-28 14:05:31 -0700

    Added package and updated attributes

---
+++ attributes.js
@@ -0,0 +1,20 @@
+module.exports = {
+  status: {
+    active: 916,
+    inactive: 918
+  },
+  externalSource: 40152,
+  cpu: 40149,
+  adapter0Name: 40948,
+  cyder: 40150,
+  mac0: 40154,
+  adapter1Name: 40949,
+  mac1: 40155,
+  ram: 40156,
+  operatingSystem: 40157,
+  printCNLabel: 40158,
+  primaryuser: 41474,
+  generalUse: 45474,
+  activeDirectoryOU: 46019,
+  warranty: 40159,
+};
