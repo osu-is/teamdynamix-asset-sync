39ebf2c0349d7020a7ff64825cb35aea1ae0e22d
thenellb <thenellb@oregonstate.edu>
2018-06-28 14:00:41 -0700

    Adding template config file

---
+++ example.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  tdCredentials: {
+    UserName: '',
+    Password: ''
+  }, 
+  sqlConfig: {
+    user: '',
+    password: '',
+    ip: ''
+  },
+  casperAuth: {
+    user: '',
+    pass: ''
+  },
+  casper: {
+    url: ''
+  }
+};
